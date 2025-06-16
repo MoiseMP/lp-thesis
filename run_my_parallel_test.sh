2d9df7a4113c59892eb06aac1798c219df3d4233
MoiseMP <102294279+MoiseMP@users.noreply.github.com>
2025-06-16 02:09:52 +0200

    update sh

--- run_my_parallel_test.sh
+++ run_my_parallel_test.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
 #SBATCH --job-name=renv_job
-#SBATCH --output=renv_job.%j.out
-#SBATCH --error=renv_job.%j.err
+#SBATCH --output=logs_job.%j.out
+#SBATCH --error=logs_job.%j.err
 #SBATCH --time=01:00:00
 #SBATCH --cpus-per-task=48
 #SBATCH --mem=64G
+
+#SBATCH --mail-user=m.n.mpongo@student.vu.nl
+#SBATCH --mail-type=END,FAIL
 
 module load 2024
 module load R/4.4.2-gfbf-2024a
@@ -13,4 +16,4 @@ module load R/4.4.2-gfbf-2024a
 R -e "renv::restore()"
 
 # Run your analysis
-Rscript scripts/my_analysis.R
+Rscript scripts/my_analysis.R ar iid 10 10
